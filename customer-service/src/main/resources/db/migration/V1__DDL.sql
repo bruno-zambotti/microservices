01fa237f24a28c40a2a3a9f771fe3e8798af2ddc
bruno-zambotti <bruno_zambotti@hotmail.com>
2020-11-02 19:02:30 -0300

    :arrow_up: Adiciona flyway migration

---
+++ customer-service/src/main/resources/db/migration/V1__DDL.sql
@@ -0,0 +1,46 @@
+create table address (
+   id integer not null,
+    city varchar(255),
+    complement varchar(255),
+    country varchar(255),
+    number integer,
+    postal_code varchar(255),
+    province varchar(255),
+    street varchar(255),
+    type varchar(255),
+    customer_id integer,
+    primary key (id)
+);
+
+create table customer (
+   id integer not null,
+    birth_date timestamp,
+    gender char(255) not null,
+    name varchar(255),
+    surname varchar(255),
+    primary key (id)
+);
+
+
+create table phone (
+   customer_id integer not null,
+    phones varchar(255)
+);
+
+create table user_auth (
+   id integer not null,
+    password varchar(255),
+    username varchar(255),
+    primary key (id)
+);
+
+
+alter table address
+   add constraint FK93c3js0e22ll1xlu21nvrhqgg
+   foreign key (customer_id)
+   references customer;
+
+alter table phone
+   add constraint FKgvgc4f2c3qkiswyv6bbs0br1p
+   foreign key (customer_id)
+   references customer;
---
+++ customer-service/src/main/resources/db/migration/V2__DML.sql
@@ -0,0 +1,9 @@
+INSERT INTO USER_AUTH (
+    id,
+    username,
+    password
+) VALUES (
+    1,
+    'admin',
+    '$2a$10$dA75iY7Yxw5G/FwOpW1VzucNIvrS9VVbU5gXWPskQRLmqlJx6uw16'
+);
